505d2a951ef512f052d63abddea28f36d37183c4
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 <mok-github@unreachable.com>
2024-06-25 15:49:16 +0000

    your commit message

---
+++ backintime/timestamping04.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+program to determing time delta to birthday
+
+"""
+
+import datetime as dt
+
+def main():                                  # main function
+    currenttime=dt.datetime.now()            # get current time as currenttime
+    print(currenttime)                       # show value of currenttime
+    bday=dt.datetime(1968, 8, 22, 12, 0, 0)  # set birthday in bday as 8/22/1968 at noon
+    print(bday)                              # show value of bday
+    print("delta: ",currenttime-bday)        # show difference between currenttime and bday
+
+if __name__ == '__main__':
+    main()
