da85a70278067e44a0e1f87585e903afcfb52e4d
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 <mok-github@unreachable.com>
2024-06-25 16:53:58 +0000

    open API research

--- backintime/timestamping04.py
+++ backintime/timestamping04.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""
-program to determing time delta to birthday
 
+"""
+Author: Michael O'Keefe
+
+program to determing time delta to birthday
 """
 
 import datetime as dt
 
-def main():                                  # main function
+def main():
+    """main function"""
+
     currenttime=dt.datetime.now()            # get current time as currenttime
     print(currenttime)                       # show value of currenttime
     bday=dt.datetime(1968, 8, 22, 12, 0, 0)  # set birthday in bday as 8/22/1968 at noon
